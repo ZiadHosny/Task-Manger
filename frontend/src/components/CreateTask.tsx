f608b7de1069713c42849e10cbae6b87c81185e1
ziad <ziad.hosny@bitspire.ca>
2023-07-28 04:31:49 +0300

    add tabel and create task

---
+++ frontend/src/components/CreateTask.tsx
@@ -0,0 +1,169 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import CloseIcon from '@mui/icons-material/Close';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { loadingState, openCreateOrUpdateTaskState, taskDateState } from '../utils/atoms';
+import { Box, Checkbox, FormControlLabel, IconButton, TextField, Typography } from '@mui/material';
+import { DateComponent } from './Date';
+import { useCreateTask } from '../hooks/useCreateTask';
+import { useUpdateTask } from '../hooks/useUpdateTask';
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export const CreateTask = () => {
+    const [_, setLoading] = useRecoilState(loadingState)
+    const [taskName, setTaskName] = React.useState<string>('')
+    const [description, setDescription] = React.useState<string>('')
+    const [tags, setTags] = React.useState<string>('')
+    const [isCompleted, setIsCompleted] = React.useState<boolean>(false)
+    const [openCreateTask, setOpenCreateTask] = useRecoilState(openCreateOrUpdateTaskState)
+    const [dueDate, setDueDate] = useRecoilState(taskDateState)
+
+    const [{ loading: createLoading }, createTask] = useCreateTask()
+    const [{ loading: updateLoading }, updateTask] = useUpdateTask()
+
+    const handleClose = () => {
+        setOpenCreateTask({ open: false });
+        setIsCompleted(false)
+        setTaskName('')
+        setDescription('')
+        setDueDate(new Date())
+        setTags('')
+    };
+
+    const onClickCreateBtn = async (e: any) => {
+        e.preventDefault();
+        await createTask({ taskName, description, dueDate, tags: tags.split(',').filter((e) => e), isCompleted: false })
+    }
+
+    const onClickUpdateBtn = async (e: any) => {
+        e.preventDefault();
+        if (openCreateTask.createOrUpdate == 'update' && openCreateTask.task) {
+            await updateTask({
+                taskId: openCreateTask.task.taskId,
+                taskName,
+                description,
+                dueDate,
+                isCompleted,
+                tags: tags.split(',').filter((e) => e)
+            })
+        }
+    }
+
+    React.useEffect(() => {
+        setLoading(createLoading)
+    }, [, createLoading])
+
+    React.useEffect(() => {
+        setLoading(updateLoading)
+    }, [updateLoading])
+
+    React.useEffect(() => {
+
+        if (openCreateTask.createOrUpdate == 'update' && openCreateTask.task) {
+            const { isCompleted, taskName, description, dueDate, tags } = openCreateTask.task
+            setIsCompleted(isCompleted)
+            setTaskName(taskName)
+            setDescription(description)
+            setDueDate(dueDate)
+            setTags(tags.join(','))
+        }
+
+    }, [openCreateTask])
+
+
+    return (
+        <Dialog
+            open={openCreateTask.open}
+            TransitionComponent={Transition}
+            keepMounted
+            // onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>
+                <Typography>
+                    {openCreateTask.createOrUpdate == 'create' ? "Create New Task" : "Update Task"}
+                </Typography>
+                <IconButton
+                    aria-label="close"
+                    onClick={handleClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <Box >
+                    <TextField
+                        fullWidth
+                        type="text"
+                        label="Enter Task Name"
+                        variant="outlined"
+                        style={{ margin: '20px 0' }}
+                        required
+                        value={taskName}
+                        onChange={(e) => { setTaskName(e.target.value) }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        label="Enter Task Description"
+                        variant="outlined"
+                        style={{ marginBottom: 20 }}
+                        value={description}
+                        onChange={(e) => { setDescription(e.target.value) }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        label="Enter Tags Separated with commas"
+                        variant="outlined"
+                        style={{ marginBottom: 20 }}
+                        value={tags}
+                        onChange={(e) => { setTags(e.target.value) }}
+                    />
+                    <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <DateComponent />
+                        {openCreateTask.createOrUpdate == 'create' ?
+                            <></>
+                            :
+                            <FormControlLabel label="Completed"
+                                control={
+                                    <Checkbox color="success"
+                                        checked={isCompleted}
+                                        onChange={(_, checked) => { setIsCompleted(checked) }}
+                                    />}
+                            />
+                        }
+
+                    </Box>
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                {openCreateTask.createOrUpdate == 'create' ?
+                    <Button type='submit' onClick={onClickCreateBtn}>Create</Button> :
+                    <Button type='submit' onClick={onClickUpdateBtn}>Update</Button>
+                }
+                <Button onClick={handleClose}>Cancel</Button>
+            </DialogActions>
+        </Dialog >
+    );
+}
---
+++ frontend/src/components/TasksTable.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useTasks } from '../hooks/useTasks';
+import { Box, Button, Checkbox, Chip, IconButton, Pagination, TableSortLabel } from '@mui/material';
+import { createOrUpdateTaskState, loadingState, openCreateOrUpdateTaskState } from '../utils/atoms';
+import { useRecoilState } from 'recoil';
+import { CreateTask } from './CreateTask';
+import { useDeleteTask } from '../hooks/useDeleteTask';
+import { useCompleteTask } from '../hooks/useCompleteTask';
+
+export const TaskTable = () => {
+    const [_loading, setLoading] = useRecoilState(loadingState)
+    const [{ value, loading }, getAllTasks] = useTasks()
+    const [{ loading: taskCompleteLoading }, changeTaskCompletion] = useCompleteTask()
+    const [{ value: taskDeleted }, deleteTask] = useDeleteTask()
+    const [currentPage, setCurrentPage] = React.useState(1)
+    const [createOrUpdateTask, setCreateOrUpdateTask] = useRecoilState(createOrUpdateTaskState)
+    const [_, setOpenCreateTask] = useRecoilState(openCreateOrUpdateTaskState)
+
+    const deleteTaskBtn = (id: string) => {
+        deleteTask(id)
+    }
+
+    React.useEffect(() => {
+        getAllTasks({})
+    }, [])
+
+    React.useEffect(() => {
+        setLoading(loading)
+    }, [loading])
+
+    React.useEffect(() => {
+        setLoading(taskCompleteLoading)
+    }, [taskCompleteLoading])
+
+    React.useEffect(() => {
+        getAllTasks({ page: currentPage })
+        setCreateOrUpdateTask(false)
+    }, [taskDeleted, createOrUpdateTask, currentPage])
+
+    const TableTitle = ({ label, sort }: { label: string, sort: string }) => {
+
+        return (
+            <TableSortLabel
+                onClick={() => { getAllTasks({ sort }) }}
+            >
+                {label}
+            </TableSortLabel>
+        )
+    }
+
+    return (
+        <>
+            <Box style={{ margin: 30, display: 'flex', justifyContent: 'flex-end' }}>
+                <Button variant='contained' onClick={() => { setOpenCreateTask({ createOrUpdate: 'create', open: true }) }}> Create Task</Button>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead style={{ background: 'black' }}>
+                        <TableRow>
+                            <TableCell align="center">Task Name</TableCell>
+                            <TableCell align="center">Description</TableCell>
+                            <TableCell align="center">
+                                <TableTitle label='Due Date' sort={'dueDate'} />
+                            </TableCell>
+                            <TableCell align="center">
+                                <TableTitle label='Is Completed' sort={'-isCompleted'} />
+                            </TableCell>
+                            <TableCell align="center">Tags</TableCell>
+                            <TableCell align="center">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {value?.tasks?.map((task) => (
+                            < TableRow
+                                key={task.taskId}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell align="center">{task.taskName}</TableCell>
+                                <TableCell align="center">{task.description}</TableCell>
+                                <TableCell align="center">{task.dueDate.toString().split('T').join(' | ').split('.')[0]}</TableCell>
+                                <TableCell align="center">
+                                    <Checkbox color="success"
+                                        checked={task.isCompleted}
+                                        onChange={() => {
+                                            changeTaskCompletion({ taskId: task.taskId, isCompleted: !task.isCompleted })
+                                        }}
+
+                                    />
+                                </TableCell>
+                                <TableCell align="center" style={{ maxWidth: 150 }}>
+                                    {task.tags.length > 0 ?
+                                        task.tags.map((tag) => < Chip label={tag}/>)
+                                        :
+                                        <></>
+                                    }
+                                </TableCell>
+                                <TableCell align="center">
+                                    <IconButton
+                                        onClick={() => deleteTaskBtn(task.taskId)}
+                                    >
+                                        <DeleteIcon style={{ color: 'white' }} />
+                                    </IconButton>
+                                    <IconButton
+                                        onClick={() => setOpenCreateTask({ createOrUpdate: 'update', open: true, task })}
+                                    >
+                                        <EditIcon style={{ color: 'white' }} />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
+            </TableContainer >
+            {value?.count && value?.count > 5 ?
+                <Pagination
+                    style={{ padding: 5, display: 'flex', justifyContent: 'flex-end' }}
+                    count={Math.ceil(value.count / 5)}
+                    onChange={(_, page) => { setCurrentPage(page); getAllTasks({ page }) }}
+                />
+                :
+                <></>
+            }
+            <CreateTask />
+
+        </>
+    );
+}
