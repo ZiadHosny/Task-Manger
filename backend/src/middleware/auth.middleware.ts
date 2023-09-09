d8216facbef8c53906dc6cd12a9d785b6f044a06
ziad <ziad.hosny@bitspire.ca>
2023-09-09 22:30:58 +0300

    add auth middleware

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,36 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { getFromEnv } from "../utils/getFromEnv.js";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/AppError.js";
+import { UserModel } from "../databases/models/user.model.js";
+import { catchAsyncError } from "../utils/catchAsyncError.js";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user: any
+        }
+    }
+}
+
+export const userAuth = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+
+    const { secretKey } = getFromEnv()
+    // Read JWT from the 'jwt' cookie
+    const token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, secretKey) as JwtPayload
+            req.user = await UserModel.findById(decoded.userId).select('-password');
+            next()
+        }
+        catch (err) {
+            console.error(err);
+            return next(new AppError('Not authorized, token failed', 401))
+        }
+    }
+    else {
+        return next(new AppError('Not authorized, no token', 401))
+    }
+})
